261990edf939f18f1b5cd924783ea8bc5405b62a
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-01-26 16:24:41 +0000

    FEAT: Put functionality for PostDetail view

--- posts/views.py
+++ posts/views.py
@@ -67,10 +67,17 @@ class PostDetail(APIView):
         serializer = PostSerializer(post, context={'request': request})
         return Response(serializer.data)
 
-    def put(self, request):
+    def put(self, request, pk):
         """
         put method used to handle HTTP PUT request for this view and update a post
         """
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     def delete(self, request):
         """
         delete method used to handle HTTP DELETE request for this view
