4065060af9c743cc9c9e6c20236b5b5fba1542af
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-01-26 12:17:17 +0000

    FEAT: Serializer for the Post model

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,9 +6,13 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     # Above overwrites owner to be username and not id (as it is in profile model)
     is_owner = serializers.SerializerMethodField()
-    # Above adds an extra field that is nopt part of the database
+    # Above the SerializerMethodField allows definition of a custom method ('get_is_owner)
+    # to determine the value of the field. 
 
     def get_is_owner(self, obj):
+        """
+        A custom method that is then used to determine the value of custom field is_owner
+        """
         request = self.context['request']
         return request.user == obj.owner
 
