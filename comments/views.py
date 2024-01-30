adf82a458d4bfc60e25b35f1c0aea2bc7f995791
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-01-30 13:25:09 +0000

    FEAT: Views and urls for comments app

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    ListCreateAPIView: Provides get and post method handlers.
+    It also automatically sends the request through to the serializer as part of the context.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+    # Above queryset is used to define which records need pulling from the DB
+
+    def perform_create(self, serializer):
+        """
+        This methiod is a hook provided by the 'CreateAPIView' and 'CreateModelMixin' classes
+        The later being extend by ListCreateAPIView. It allows for customisation
+        of the new object before it is saved to the database.
+        """
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    RetrieveUpdateDestroyAPIView: Used for read-write-delete endpoints to represent a single model instance.
+    Provides get, put, patch and delete method handlers.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
