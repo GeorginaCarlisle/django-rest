e322ca14cc35485a100548c4ba2d59b5bfb4743b
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-01-30 13:41:11 +0000

    FEAT: Add like model

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Like(models.Model):
+    """
+    Like model, related to User and Post
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """
+        unique_together is used as a constraint tto specify that
+        the given combinations of fields should be unique.
+        """
+        ordering = ['-created_at']
+        unique_together = ['owner', 'post']
+
+    def __str__(self):
+        return f"{self.owner}'s like on {self.post}"
+    
