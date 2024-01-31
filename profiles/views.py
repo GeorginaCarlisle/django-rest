0e67781979c13be1c35dffa35223d40846c17cc4
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-01-31 11:57:35 +0000

    FEAT: Add extra count fields to profile detail

--- profiles/views.py
+++ profiles/views.py
@@ -34,4 +34,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    )
