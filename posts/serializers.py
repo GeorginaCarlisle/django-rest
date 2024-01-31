e10c57ed21b61223a50f39f8ad156c901e06ac4e
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-01-31 11:20:54 +0000

    FEAT: Add like.id field to post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         """
@@ -31,10 +33,23 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_like_id(self, obj):
+        """
+        A custom method to determine the like_id value
+        It checks if the logged in user is the owner of a like object where
+        the post is this post. If so like_id is given the id of this like object.
+        """
+        user = self.context['request'].user
+        if user.is_authenticated:
+            liked = Like.objects.filter(owner=user, post=obj). first()
+            return liked.id if liked else None
+        return None
 
     class Meta:
         model = Post
         fields = [
             'owner', 'created_at', 'updated_at', 'title', 'content', 
             'image', 'is_owner', 'profile_id', 'profile_image', 'image_filter',
+            'like_id',
         ]
